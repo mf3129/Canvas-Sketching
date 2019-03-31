b5a46bc582597251b511bacf7c68a40bbbee4d8e
Makan Fofana <mfofanaT99@gmail.com>
2019-03-31 16:55:34 -0400

    Added functionality to draw line on canvas.

--- DrawingOnCanvas/ViewController.swift
+++ DrawingOnCanvas/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class Canvas: UIView {
     
+    
+    //Canvas Methods
+    
     override func draw(_ rect: CGRect) {
         //Custom Drawing Section
         super.draw(rect)
@@ -17,26 +20,45 @@ class Canvas: UIView {
         guard let context = UIGraphicsGetCurrentContext() else
         {return}
         
+        //Example/Dummy Points
         //Line to be drawn
-        let startingPoint = CGPoint(x: 0, y: 0)
-        let endingPoint = CGPoint(x: 100, y: 100)
         
-        context.move(to: startingPoint)
-        context.addLine(to: endingPoint)
+//        let startingPoint = CGPoint(x: 0, y: 0)
+//        let endingPoint = CGPoint(x: 100, y: 100)
+//
+//        context.move(to: startingPoint)
+//        context.addLine(to: endingPoint)
+        
+        
+        for (i, firstPoint) in line.enumerated() {
+            if i == 0 {
+                context.move(to: firstPoint)
+            } else {
+                context.addLine(to: firstPoint)
+            }
+        }
+        
         
         context.strokePath()
-        
     }
+    
+    
+    
+    var line = [CGPoint]()
     
     //Tracking the first finger touch
     override func touchesMoved(_ touches: Set<UITouch>, with: UIEvent?) {
         guard let point = touches.first?.location(in: nil) else
         {return}
-        print(point)
+        //print(point)
+        line.append(point)
+        
+        setNeedsDisplay()
     }
-    
-    
+
 }
+
+
 
 class ViewController: UIViewController {
     
