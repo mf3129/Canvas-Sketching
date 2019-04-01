de08d6a08311dd9c549ab77b0a54d7d4875d7fdf
Makan Fofana <mfofanaT99@gmail.com>
2019-04-01 15:18:12 -0400

    Created Undo and Clear Button Programmatically. Assigned Constraints to each in Stack View.

--- DrawingOnCanvas/ViewController.swift
+++ DrawingOnCanvas/ViewController.swift
@@ -8,85 +8,44 @@
 
 import UIKit
 
-class Canvas: UIView {
-    
-    
-    //Canvas Methods
-    
-    override func draw(_ rect: CGRect) {
-        //Custom Drawing Section
-        super.draw(rect)
-        
-        guard let context = UIGraphicsGetCurrentContext() else
-        {return}
-        
-        //Example/Dummy Points
-        //Line to be drawn
-        
-//        let startingPoint = CGPoint(x: 0, y: 0)
-//        let endingPoint = CGPoint(x: 100, y: 100)
-//
-//        context.move(to: startingPoint)
-//        context.addLine(to: endingPoint)
-        
-        //Defning Properites Of The Line
-        context.setStrokeColor(UIColor.blue.cgColor)
-        context.setLineWidth(10)
-        context.setLineCap(.butt)
-        
-        lines.forEach { (line) in
-            for (i, firstPoint) in line.enumerated() {
-                if i == 0 {
-                    context.move(to: firstPoint)
-                } else {
-                    context.addLine(to: firstPoint)
-                }
-            }
-        }
-        
-        
-        context.strokePath()
-    }
-    
-    
-    var lines = [[CGPoint]]()
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        lines.append([CGPoint]()) //Adding a new line to the lines array when a new touch is detected.
-    }
-    
-    
-    //Tracking the first finger touch
-    override func touchesMoved(_ touches: Set<UITouch>, with: UIEvent?) {
-        guard let point = touches.first?.location(in: nil) else
-        {return}
-        //print(point)
-        
-        guard var lastLine = lines.popLast() else {return}
-        lastLine.append(point)
-        lines.append(lastLine)
-
-        
-        //line.append(point)
-        
-        setNeedsDisplay()
-    }
-
-}
-
-
 
 class ViewController: UIViewController {
     
     let canvas = Canvas()
     
+    let undoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Undo", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        return button
+    }()
+    
+    let clearButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Clear", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        return button
+    }()
+    
+    
+    override func loadView() {
+        self.view = canvas  //Setting the view equal to the canvas view.
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.addSubview(canvas)
         canvas.backgroundColor = .white
-        canvas.frame = view.frame
+        
+        let stackView = UIStackView(arrangedSubviews: [undoButton, clearButton])
+       
+       stackView.distribution = .fillEqually
+       stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
     }
+    
 
 
 }
